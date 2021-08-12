8e9e9d0d41f84fbd2d46e80206983445c623ff2f
ella thomas <78177131+ellalthomas@users.noreply.github.com>
2021-08-12 21:58:20 +0000

    Basic Game works. Points get added when the guess is correct.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 console.log('Welcome to the Card Game. In this game a card will be dealt. Your job is to guess if the next card will be higher or lower. You have will have 3 rounds. Who ever has the most points at the end of the rounds will win. Good luck. ')
 
 //list of numbers to be picked
-let number = [' Ace', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9', ' 10', ' Jack', ' Queen', ' King'];
+let number = [ 1,  2,  3,  4,  5,  6,  7,  8,  9,  10,  11,  12,  13];
 
 //Picks a card
 let length = number.length
@@ -16,28 +16,32 @@ let randomCard1 = Math.floor(Math.random() * length + 0);
 let tableCard1 = number[randomCard1];
 let tableCard2 = number[randomCard2];
 
+let point1 = 0
+
 console.log(' These are the cards you will choose from. \n' + number)
 
 //Lets player know what card is dealt
-console.log('The first card is a' + tableCard1)
+console.log('The first card is a ' + tableCard1)
 
-let higher = ' '
-let lower = ' '
 let player1 = prompt('Player 1, do you think the next card dealt will be higher or lower?')
 
-console.log('The card dealt is a' + tableCard2)
+console.log('The card dealt is a ' + tableCard2)
 
 //if next card is higher and player1 guessed higher, give a point
 //if next card is lower and player1 guessed lower, give point
 //if next card is higher and player1 guessed lower, no point
 //if next card is lower and player1 guessed higher, no point
 
-if (tableCard1 < tableCard2 || player1 == higher) {
+if (tableCard1 < tableCard2 && player1 == 'higher') {
   console.log('You guessed correctly')
-} else if (tableCard1 > tableCard2 || player1 == higher) {
+  point1 ++
+} else if (tableCard1 > tableCard2 && player1 == 'higher') {
   console.log('You guessed incorrectly')
-}else if(tableCard1 > tableCard2 || player1 == lower) {
+} else if (tableCard1 < tableCard2 && player1 == 'lower') {
   console.log('You guessed incorrectly')
-}else if(tableCard1 < tableCard2 || player1 == lower) {
+}else if(tableCard1 > tableCard2 && player1 == 'lower') {
   console.log('You guessed correctly')
+  point1 ++
 }
+console.log('Player1 you have ' + point1 + ' points.')
+
